3554a6527482e20be42b9784c1d25199e1430a6f
Marvel2456 <marvellousidemudia@gmail.com>
2023-08-12 15:14:24 +0100

    new migration

--- ralyn/ecom/models.py
+++ ralyn/ecom/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
 
 User = get_user_model()
 
 # Create your models here.
 
 class Category(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     name = models.CharField(max_length=225, blank=True, null=True)
 
     class Meta:
@@ -15,6 +17,7 @@ class Category(models.Model):
         return self.name
 
 class Item(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     name = models.CharField(max_length=255, blank=True, null=True)
     image = models.ImageField(upload_to='item/image', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
@@ -26,11 +29,37 @@ class Item(models.Model):
         return self.name
 
 
-class OrderItem(models.Model):
-    pass
-
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    Ordered_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+    transaction_id = models.CharField(max_length=100, null=True)
 
+    def __str__(self):
+        return self.user.email
+
+class OrderItem(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    item = models.ForeignKey(Item, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.PositiveIntegerField(default=0, blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item.name
 
 # Add is popular boolean field to the product to create a list of all the popular products
+
+class ShippingDetail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=225, blank=True, null=True)
+    city = models.CharField(max_length=225, blank=True, null=True)
+    state = models.CharField(max_length=225, blank=True, null=True)
+    zipcode = models.CharField(max_length=225, blank=True, null=True)
+    phone = models.CharField(max_length=225, blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
--- ralyn/ecom/views.py
+++ ralyn/ecom/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import *
 # Create your views here.
 
 def index(request):
-    pass
+    item = Item.objects.all()
+
+    context = {
+        'item':item,
+    }
+    return render(request, 'ecom/index.html', context)
