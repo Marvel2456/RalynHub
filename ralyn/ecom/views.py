32cd9944363d260d69895b3e7047cca46d94fe4f
Marvel2456 <marvellousidemudia@gmail.com>
2023-08-24 01:11:37 +0100

    updated django allauth template

--- ralyn/ecom/views.py
+++ ralyn/ecom/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from . models import *
 # Create your views here.
 
-def index(request):
+def Index(request):
     number_of_products = 10
     products = Item.objects.all()[:number_of_products]
 
@@ -10,3 +10,27 @@ def index(request):
         'products':products,
     }
     return render(request, 'ecom/index.html', context)
+
+def About(request):
+    return render(request, 'ecom/about.html')
+
+def Products(request):
+    return render(request, 'ecom/products.html')
+
+def Cart(request):
+    if request.user.is_authenticated:
+        customer = request.user
+        order, created = Order.objects.get_or_create(customer=customer, completed=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {
+        'items':items
+    }
+    return render(request, 'ecom/cart.html', context)
+
+def Checkout(request):
+    pass
+
+def Contact(request):
+    return render(request, 'ecom/contact.html')
