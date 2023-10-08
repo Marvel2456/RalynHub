5c969019a6b66c184f8ffb3716f94f6b93d72423
Marvel2456 <marvellousidemudia@gmail.com>
2023-10-08 07:06:25 +0100

    added shipping info in admin added contact info in the admin fixed the side bar for admin

--- ralyn/erm/urls.py
+++ ralyn/erm/urls.py
@@ -13,5 +13,11 @@ urlpatterns = [
    path('delete_product/', views.deleteProduct, name='delete_product'),
    path('edit_category/', views.EditCategory, name='edit_category'),
    path('delete_category/', views.deleteCategory, name='delete_category'),
-   path('order/', views.Orders, name='order'),
+   path('payment_history/', views.orderHistory, name='payment_history'),
+   path('contact_list/', views.contacts, name='contact_list'),
+   path('contact_detail/<str:uuid>/', views.contactDetail, name='contact_detail'),
+   path('shipping/', views.ShippingAddress, name='shipping'),
+   path('shipping_detail/<str:uuid>/', views.shippingDetail, name='shipping_detail'),
+   path('delete_contact/', views.deleteContact, name='delete_contact'),
+   path('delete_shipping/', views.deleteShipping, name='delete_shipping'),
 ]
